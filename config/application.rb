783248b2a8471c7af3c62a1bbd126741b1fea0a2
Matias Nieves <matinieves@gmail.com>
2017-05-07 21:33:42 -0300

    Change application name. Also:  * Remove some gems.  * Add API configuration.

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PageScrapper
+module PageScraper
   class Application < Rails::Application
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    config.autoload_paths << "#{Rails.root}/lib"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
